f7effe8fce9c030e19968834de66f67d9188c2f5
Arieleon25 <Arieleon25@yahoo.com>
2023-02-08 18:39:42 -0500

    Project2_Functions Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+//Created Button Function//
+function My_First_Function() {
+    var str = "Nice Job!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+//Created operator += Function//
+function myFunction() {
+    var sentence ="I am learning";
+    sentence +=" a lot from this bootcamp!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
