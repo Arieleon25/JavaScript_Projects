80d77cb52589003cb92185003835be32559eddc1
Arieleon25 <Arieleon25@yahoo.com>
2023-02-08 18:10:30 -0500

    Basic Javascript Submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+var A = "Aqui todo chilling"; //Creating a variable to add in a window.alert//
+window.alert(A);
+document.write("Rigi told Arie,\"STOP STUDYING SO MUCH\" <br> \"No way\" Arie responded."); //Added a backlash//
+document.write(" I have been impressed with the urgency of doing." + " Knowing is not enough; we must apply." +  " Being willing is not enough; we must do. " + "-Leonardo Da Vinci   "); //Contenating a string//
+
+var B = "Don/'t " + "stop";
+
+document.write(B);
+
+var myFamily = " Eskinazi ", Dad = " Izack ", Mom = " Anat ", Me = " Arie ", MB = " Rafa ", LB= " Eliel "; //Assigned multiple variables//
+
+document.write(MB);
+
+document.write(5+5); //Expressions//
+
+//Comment Practice//
+
+//Submission assignmnent: Creating two variables//
+var sent1 = " This is the beginning of the string "; 
+var sent2 = "and this is the end of the string";
+
+var sentmax = sent1 + sent2;
+
+document.write (sentmax)
